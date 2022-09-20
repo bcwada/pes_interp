428a809d733c4187cfbeb9be7e3184220c91a8b8
D. Williams <dmgw@stanford.edu>
2022-09-20 15:10:01 -0700

    Added minor documentation.

--- doc/examples/init_test/run.sh
+++ doc/examples/init_test/run.sh
@@ -1,2 +1,9 @@
 export PYTHONPATH="$PYTHONPATH:/Projects/pes_interp/pes_interp"
+mkdir output
+
+# arguments input PES file dir
+# input TC dir
+# initial conditions dir
+# # timesteps
+# output TC calculations
 python /Projects/pes_interp/pes_interp/script.py pes_files/ tc_files/ init_conds/ 1 1 output
